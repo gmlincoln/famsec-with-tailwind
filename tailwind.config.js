cff40131c0a809d868de12a25b770aaafad84cf5
gmlincoln <maula03@gmail.com>
2023-08-11 21:51:50 +0600

    banner-section-added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,83 @@ module.exports = {
     },
     plugins: [],
 }
+/*
+<div class="flex flex-col lg:flex-row gap-20">
+          <div class="w-5/12 space-y-6">
+            <h4 class="text-[#323E43]">Welcome to FamSec</h4>
+            <h2 class="text-5xl font-extrabold text-[#233036]">Protecting Families,<br>
+               Securing Futures</h2>
+            <p>
+              We understand that protecting your loved ones is of utmost importance. That's why we are dedicated to providing comprehensive solutions tailored to your family's unique needs. Our expert team is committed to creating a solid foundation of security and peace of mind for you and your loved ones.
+            </p>
+            <button class="btn bg-[#1D5B79] text-white capitalize hover:bg-[#134761] ">Get started</button>
+            <button class="btn bg-[#2EC0BB] text-white capitalize hover:bg-[#179792] ">How it works?</button>
+          </div>
+          <div class="grid grid-cols-3 gap-3">
+            <img class="rounded-lg row-span-2 w-[100%]" src="images/hero-img-1-min.jpg" alt="">
+            <img class="rounded-lg lg:mt-20 w-20 h-20" src="images/hero-img-2-min.jpg" alt="">
+            <div class="bg-[#FFF3D9] w-20 h-20 mt-20 rounded-tr-full"></div>
+            <div class="w-40 col-span-2">
+              <img class="rounded-lg " src="images/hero-img-3-min.jpg" alt="">
+            </div>
+           <div class="w-20 h-20">
+            <img class="rounded-lg lg:ml-24" src="images/hero-img-4-min.jpg" alt="">
+           </div>
+            <img class="rounded-lg w-56 col-span-2" src="images/hero-img-5-min.jpg" alt="">
+          </div>
+        </div>
+        
+        
+        <div class="flex flex-row gap-20 justify-between">
+          <div style="height: 200px; width: 200px; background: #000;" class="w-5/12" >
+
+          </div>
+          <div style="height: 200px; width: 200px; background:#Adc;" class="w-5/12">
+
+          </div>
+        </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="flex flex-col lg:flex-row gap-10">
+          <div class="space-y-6">
+            <h4 class="text-[#323E43]">Welcome to FamSec</h4>
+            <h2 class="text-5xl font-extrabold text-[#233036]">Protecting Families,<br>
+               Securing Futures</h2>
+            <p>
+              We understand that protecting your loved ones is of utmost importance. That's why we are dedicated to providing comprehensive solutions tailored to your family's unique needs. Our expert team is committed to creating a solid foundation of security and peace of mind for you and your loved ones.
+            </p>
+            <button class="btn bg-[#1D5B79] text-white capitalize hover:bg-[#134761] ">Get started</button>
+            <button class="btn bg-[#2EC0BB] text-white capitalize hover:bg-[#179792] ">How it works?</button>
+          </div>
+          <div class="grid grid-cols-2 gap-3 justify-between">
+            <img class="rounded-lg row-span-3" src="images/hero-img-1-min.jpg" alt="">
+            <div class="flex flex-row gap-2 lg:mt-16">
+              <img class="rounded-lg w-20 h-20" src="images/hero-img-2-min.jpg" alt="">
+              <div class="bg-[#FFF3D9] w-20 h-20  rounded-tr-full"></div>
+            </div>
+            <img class="rounded-lg w-44" src="images/hero-img-3-min.jpg" alt="">
+           <div class="lg:ml-16">
+            <img class="rounded-lg w-20 h-20" src="images/hero-img-4-min.jpg" alt="">
+            <div class="bg-[#CCF0EE] w-20 h-20  rounded-bl-full"></div>
+           </div>
+            <img class="rounded-lg w-56 col-span-2" src="images/hero-img-5-min.jpg" alt="">
+        </div>
+
+
+
+
+
+        */ 
